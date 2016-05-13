1b803a406ccaa58a5b82b73c267e55ad3eb0a839
tech-camp <tech-camp@tech-camp-128-no-MacBook-Air.local>
2016-05-13 18:54:02 +0900

    progressbarまで

---
+++ app/assets/javascripts/progressbar.js
@@ -0,0 +1,50 @@
+$(function() {
+  var $pb = $('.progress-bar:first');
+  var $pb2 = $('.progress-bar:last');
+  $("#items :checkbox").click(function() {
+    if($("#items :checked").length == 1) {
+      $pb.attr({
+        'style':'width:33%;',
+        'class':'progress-bar'
+      }).html(" 33% ");
+      } else if($("#items :checked").length == 2) {
+      $pb.attr({
+        'style':'width:66%;',
+        'class':'progress-bar'
+      }).html(" 66% ");
+      } else if($("#items :checked").length == 3) {
+      $pb.attr({
+        'style':'width:100%;',
+        'class':'progress-bar progress-bar-striped active'
+      }).html(" 100% ");
+      } else {
+      $pb.attr({
+        'style':'width:0%;',
+        'class':'progress-bar'
+      }).html(" 0% ");
+    }
+  });
+  $("#items2 :checkbox").click(function() {
+    if($("#items2 :checked").length == 1) {
+      $pb2.attr({
+        'style':'width:33%;',
+        'class':'progress-bar'
+      }).html(" 33% ");
+      } else if($("#items2 :checked").length == 2) {
+      $pb2.attr({
+        'style':'width:66%;',
+        'class':'progress-bar'
+      }).html(" 66% ");
+      } else if($("#items2 :checked").length == 3) {
+      $pb2.attr({
+        'style':'width:100%;',
+        'class':'progress-bar progress-bar-striped active'
+      }).html(" 100% ");
+      } else {
+      $pb2.attr({
+        'style':'width:0%;',
+        'class':'progress-bar'
+      }).html(" 0% ");
+    }
+  });
+  });
