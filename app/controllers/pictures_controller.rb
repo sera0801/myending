2cef370e1578e28386e8d0c2d3274ae279a4f941
tech-camp <tech-camp@tech-camp-128-no-MacBook-Air.local>
2016-05-19 22:01:06 +0900

    写真のvalidation追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
     layout 'users_mypage'
+    before_action :set_user, only: :create
 
     def index
     end
@@ -10,7 +11,11 @@ class PicturesController < ApplicationController
     end
 
     def create
-      Picture.create(picture_params)
+      @picture = current_user.pictures.create(picture_params)
+      if @picture.errors.any?
+        render :new
+      end
+
     end
 
     def destroy
@@ -23,6 +28,10 @@ class PicturesController < ApplicationController
     end
 
     private
+
+    def set_user
+      @user = current_user
+    end
     def picture_params
       params.require(:picture).permit(:title, :portrait).merge(user_id: current_user.id)
     end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,25 @@
 class Picture < ActiveRecord::Base
 
+# validates :portrait,  presence: true
+
+validate :add_error_sample
+
+def add_error_sample
+    # nameが空のときにエラーメッセージを追加する
+    if portrait.blank?
+      errors[:base] << "写真は必ず選択して下さい"
+    end
+
+    # 価格が空のときにエラーメッセージを追加する
+    # if price.blank?
+    #   errors[:base] << "価格は必ず入力して下さい"
+    # end
+  end
+
+
+
+
+
 belongs_to :user
 
 has_attached_file :portrait,
@@ -7,3 +27,5 @@ has_attached_file :portrait,
 validates_attachment_content_type :portrait,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
 end
+
+
